5a84b9f00dc7368db36972c186db34cfcd0f1864
Erica M. ("Loonatic") <l.oony@aol.com>
2022-11-03 03:26:20 -0500

    egg2bam: Use env var panda path, if there is one

--- scripts/egg2bam.py
+++ scripts/egg2bam.py
@@ -3,12 +3,14 @@ import subprocess
 import sys
 import time
 
-
 sys.argv.pop(0)
 
+pandaDir = os.environ.get("PANDA_PATH_1") + "\\bin\\" if os.environ.get("PANDA_PATH_1") else ""
+print(f"Panda Directory = {pandaDir}")
 
 for entry in sys.argv:
     if entry.endswith(".egg"):
-        subprocess.call(['egg2bam.exe', entry, '-o', entry.replace('.egg', '.bam')])
+        subprocess.call([f"{pandaDir}egg2bam.exe", entry, '-o', entry.replace('.egg', '.bam')])
+    # Give some time for the user to see the output before closing
     time.sleep(3)
 
